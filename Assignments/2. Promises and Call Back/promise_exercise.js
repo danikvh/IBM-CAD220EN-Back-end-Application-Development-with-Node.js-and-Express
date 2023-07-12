8f4757bcc374903d60998fd988d8079eefa6019e
danikvh <danivicen@gmail.com>
2023-07-12 21:58:33 +0200

    Promises

---
+++ Assignments/2. Promises and Call Back/promise_exercise.js
@@ -0,0 +1,45 @@
+// Create a script which has two methods that return promises - One of the promises should get reolved
+// after 6 seconds timeout and the other one after 3 seconds timeout. Call the promise in such a way
+// that the second promise is invoked after the first promise is resolved.
+
+let myPromise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promise 1 resolved")
+    }, 6000)
+})
+
+let myPromise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promise 2 resolved")
+    }, 3000)
+})
+
+//Console log before calling the promise
+console.log("Before calling promise");
+
+//Call the promise and wait for it to be resolved and then print a message.
+myPromise1.then((successMessage) => {
+    console.log("From Callback " + successMessage)
+    myPromise2.then((successMessage) => {
+        console.log("From Callback " + successMessage)
+    })
+})
+
+//Console log after calling the promise
+console.log("After calling promise");
+
+
+
+// Change the code to call the promises sequentially and see how the output changes.
+//Console log before calling the promise
+console.log("#Before calling promise");
+
+myPromise1.then((successMessage) => {
+    console.log("#From Callback " + successMessage)
+})
+myPromise2.then((successMessage) => {
+    console.log("#From Callback " + successMessage)
+})
+
+//Console log after calling the promise
+console.log("#After calling promise");
