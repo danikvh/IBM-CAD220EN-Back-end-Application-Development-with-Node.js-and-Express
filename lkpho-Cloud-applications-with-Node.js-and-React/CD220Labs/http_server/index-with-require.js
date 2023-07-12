657d1e16583afe0ceb65de1541eac9c8c7996f11
danikvh <danivicen@gmail.com>
2023-07-12 20:14:04 +0200

    Exercise 1

--- lkpho-Cloud-applications-with-Node.js-and-React/CD220Labs/http_server/index-with-require.js
+++ lkpho-Cloud-applications-with-Node.js-and-React/CD220Labs/http_server/index-with-require.js
@@ -3,7 +3,18 @@ const today = require('./today');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
+  let date = today.getDate();
+  let greeting = "It is still not morning";
+  if (date.getHours()>6 && date.getHours()<12) {
+    greeting = "Good morning!"
+  } else if (date.getHours()>=12 && date.getHours()<18) {
+    greeting = "Good afternoon!"
+  } else if (date.getHours()>=18 && date.getHours()<21) {
+    greeting = "Good evening!"
+  } else if (date.getHours()>=21 && date.getHours()<24) {
+    greeting = "Good night!"
+  }
+  res.end(`Hello, ${greeting}`);
 }
 
 const port = 8080;
