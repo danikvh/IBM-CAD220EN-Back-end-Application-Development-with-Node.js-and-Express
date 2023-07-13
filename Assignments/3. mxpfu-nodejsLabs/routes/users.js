1f5629e94374020aa14d5c7e62d121455ab4e057
danikvh <danivicen@gmail.com>
2023-07-13 23:08:16 +0200

    More methods

--- Assignments/3. mxpfu-nodejsLabs/routes/users.js
+++ Assignments/3. mxpfu-nodejsLabs/routes/users.js
@@ -37,9 +37,7 @@ router.get("/:email",(req,res)=>{
 
 
 //LINEA DE COMANDOS PARA PROBAR EN WINDOWS
-/* $url = "http://localhost:5000/user?firstName=Jon&lastName=Lovato&email=jonlovato@theworld.com&DOB=10/10/1995"
-
-$response = Invoke-WebRequest -Uri $url -Method Post
+/* $response = Invoke-WebRequest -Uri "http://localhost:5000/user?firstName=Jon&lastName=Lovato&email=jonlovato@theworld.com&DOB=10/10/1995" -Method Post
 $response.Content*/
 // POST request: Create a new user
 router.post("/",(req,res)=>{
@@ -53,17 +51,42 @@ router.post("/",(req,res)=>{
 });
 
 
+/* Invoke-WebRequest -Uri "http://localhost:5000/user/johnsmith@gamil.com?DOB=1/1/1971" -Method Put */
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  let filtered_users = users.filter((user) => user.email === email);
+  if (filtered_users.length > 0) {
+    let filtered_user = filtered_users[0];
+    let DOB = req.query.DOB;
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
+    //if the DOB has changed
+    if(DOB) {
+      filtered_user.DOB = DOB
+    }
+    if(firstName) {
+      filtered_user.firstName = firstName
+    }
+    if(lastName) {
+      filtered_user.lastName = lastName
+    }
+    users = users.filter((user) => user.email != email);
+    users.push(filtered_user);
+    res.send(`User with the email ${email} updated.`);
+  }
+  else{
+    res.send("Unable to find user!");
+  }
 });
 
 
+/* Invoke-WebRequest -Uri "http://localhost:5000/user/johnsmith@gamil.com" -Method Delete */
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  users = users.filter((user) => user.email != email);
+  res.send(`User with the email ${email} deleted.`);
 });
 
 module.exports=router;
