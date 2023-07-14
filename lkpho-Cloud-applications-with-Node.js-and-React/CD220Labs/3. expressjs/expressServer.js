dd4ceb05fc7b4d81368a8243ac6aab4da1137b58
danikvh <danivicen@gmail.com>
2023-07-14 13:59:59 +0200

    fetchMonth end point

--- lkpho-Cloud-applications-with-Node.js-and-React/CD220Labs/3. expressjs/expressServer.js
+++ lkpho-Cloud-applications-with-Node.js-and-React/CD220Labs/3. expressjs/expressServer.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = new express();
 
 let loginDetails = [];
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
 
 app.get("/",(req,res)=>{
     res.send("Welcome to the express server")
@@ -19,6 +21,15 @@ app.post("/login/:name",(req,res)=>{
 
 app.get("/:name",(req,res)=>{
     res.send("Hello "+req.params.name)
+})
+
+app.get("/fetchMonth/:num",(req,res)=>{
+    let num = parseInt(req.params.num)
+    if (num >= 1 & num <= 12) {
+        res.send("The month is : " + months[num-1])
+    } else {
+        res.send("Error, introduce a valid month")
+    }
 })
 
 app.listen(3333, () => {
